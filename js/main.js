8e79bdd59742bc4dd271a2bc2bc82208a8d2219f
iChristmas8 <ichristmas@yandex.ru>
2021-11-30 20:39:49 +0500

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -30,5 +30,13 @@ var reviewsSlider = new Swiper('.reviews-slider', {
     //   pageUpDown: true,
     // }  
   });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function() {
+  console.log("Клик по кнопке");
+  document
+  .querySelector(".navbar-bottom")
+  .classList.toggle("navbar-bottom--visible");
+})
   
   
